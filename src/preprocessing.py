cb73dab01ced7da8d986fec23a6b97b7311bc0ac
ddlatumalea <ddlatumalea@bioinf.nl>
2022-01-25 11:22:00 +0100

    Updated preprocesing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -23,12 +23,17 @@ def get_cleaned_data_path():
 DATA_DIR = get_data_path()
 DATA_CLEANED_DIR = get_cleaned_data_path()
 
-LIFE_EXPECTANCY_FILE = 'life_expectancy_at_birth.xlsx'
+NL_LIFE_EXP_FILE = 'netherlands_life_exp.xlsx'
+JP_LIFE_EXP_FILE = 'japan_life_exp.xlsx'
+CA_LIFE_EXP_FILE = 'canada_life_exp.xlsx'
+
 COUNTRY_CODES_FILE = 'country_codes'
 POPULATION_FILE = 'pop'
 MORTALITY_FILE = 'Morticd10_part'  # append 1 till 5 and concatenate the 5 files together
 
-LIFE_EXPECTANCY_PATH = Path(DATA_DIR, LIFE_EXPECTANCY_FILE)
+NL_LIFE_EXP_PATH = Path(DATA_DIR, NL_LIFE_EXP_FILE)
+JP_LIFE_EXP_PATH = Path(DATA_DIR, JP_LIFE_EXP_FILE)
+CA_LIFE_EXP_PATH = Path(DATA_DIR, CA_LIFE_EXP_FILE)
 COUNTRY_CODES_PATH = Path(DATA_DIR, COUNTRY_CODES_FILE)
 POPULATION_PATH = Path(DATA_DIR, POPULATION_FILE)
 MORTALITY_PATH = Path(DATA_DIR, MORTALITY_FILE)
@@ -38,13 +43,22 @@ if __name__ == '__main__':
     mortality_datasets = [Path(DATA_DIR, f"{MORTALITY_FILE}{str(i + 1)}") for i in range(0, 5)]
     mortality_datasets = [pd.read_csv(data_path) for data_path in mortality_datasets]
 
-    df_life_exp = pd.read_excel(LIFE_EXPECTANCY_PATH)
+    nl_life_exp = pd.read_excel(NL_LIFE_EXP_PATH)
+    jp_life_exp = pd.read_excel(JP_LIFE_EXP_PATH)
+    ca_life_exp = pd.read_excel(CA_LIFE_EXP_PATH)
+
     df_country_codes = pd.read_csv(COUNTRY_CODES_PATH)
     df_population = pd.read_csv(POPULATION_PATH)
     df_mortality = pd.concat(mortality_datasets)
 
+    # Append life expectation files
+    dfs = [nl_life_exp, jp_life_exp, ca_life_exp]
+
+    for df in dfs[1:]:
+        dfs[0] = dfs[0].append(df)
+
     # call cleaners
-    df_life_exp = clean_life_expectancy(df_life_exp)
+    df_life_exp = clean_life_expectancy(dfs[0])
     df_country_codes = clean_country_codes(df_country_codes)
     df_population = clean_population(df_population)
     df_mortality = clean_mortality(df_mortality)
